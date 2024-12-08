11840cf757d6dced8ad6b8cad6a656c9d0e38624
KahfiSmith <alkahfii2018@gmail.com>
2024-12-08 21:48:26 +0700

    feat(article): add article

---
+++ resources/views/livewire/pages/home/artikel.blade.php
@@ -0,0 +1,23 @@
+<section id="artikel" class="flex flex-col px-14 mb-20 pt-20">
+    <div class="flex flex-col mb-7">
+        <h2 class="text-3xl font-semibold mb-1">Memulai Peternakan Ayam Broiler</h2>
+        <p class="font-medium text-lg">Pelajari langkah-langkah awal untuk membangun peternakan ayam broiler yang
+            sukses. Dari perencanaan hingga
+            persiapan kandang, semua informasi yang Anda butuhkan ada di sini.</p>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-4">
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+        <x-card-artikel />
+    </div>
+    <div class="flex justify-end">
+        <span class="cursor-pointer hover:border-b-2 hover:border-gray-700 font-semibold hover:text-orangeCrayola transition duration-200 ease-in-out">
+            Lihat semua artikel...
+        </span>
+    </div>
+</section>
