94f872e8c2f3788f59eeaf4c8257f34cbbc7c745
KahfiSmith <alkahfii2018@gmail.com>
2025-01-30 17:59:53 +0700

    sweet alert

--- resources/views/layouts/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard-layout.blade.php
@@ -9,6 +9,8 @@
     <title>@yield('title', config('app.name', 'Laravel'))</title>
     @livewireStyles
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 </head>
 
 <body class="font-sans antialiased">
