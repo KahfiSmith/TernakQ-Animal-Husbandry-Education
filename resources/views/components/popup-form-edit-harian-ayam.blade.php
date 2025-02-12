261e5b45e96ed691082aa785a531b9d11395844e
KahfiSmith <alkahfii2018@gmail.com>
2025-02-12 21:11:39 +0700

    fix(layout): fix post data with controller

--- resources/views/components/popup-form-edit-harian-ayam.blade.php
+++ resources/views/components/popup-form-edit-harian-ayam.blade.php
@@ -1,11 +1,10 @@
-<!-- resources/views/components/popup-form-edit-harian-ayam.blade.php -->
-@props(['batches']) <!-- Mendefinisikan props 'batches' -->
-
-<div
-    x-show="openModal === 'editHarianAyam'"
-    x-cloak
-    class="fixed inset-0 flex items-center justify-center z-50 bg-black/15"
-    x-transition>
+<div x-data="{ openModal: null, editData: {} }"
+    @open-edit-harian.window="
+        openModal = 'editHarianAyam'; 
+        editData = $event.detail;
+    "
+    x-show="openModal === 'editHarianAyam'" x-cloak
+    class="fixed inset-0 flex items-center justify-center z-50 bg-black/15" x-transition>
     <div class="bg-white w-1/3 p-6 rounded-lg shadow-lg relative">
         <!-- Tombol Close -->
         <button @click="openModal = null"
@@ -17,17 +16,17 @@
         <h2 class="text-2xl font-semibold mb-4">Edit Data Harian Ayam</h2>
 
         <!-- Form Edit Harian Ayam -->
-        <form method="POST" :action="`/harian/${editData.id}`" @submit.prevent="submitEditHarian">
+        <form method="POST" :action="`/harian/${editData.id}`">
             @csrf
             @method('PUT')
+
             <div class="space-y-4 mb-8">
                 <!-- Nama Batch -->
                 <div>
                     <x-input-label for="dailyBatchName" :value="__('Nama Batch')" />
                     <select id="dailyBatchName" name="dailyBatchName"
-                        class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] focus:shadow-[2px_2px_0px_2px_#374151] focus:translate-y-0.5 focus:translate-x-0.5 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-gray-700 text-gray-700 leading-5 transition duration-150 ease-in-out block mt-1 w-full py-2.5"
-                        required
-                        x-model="editData.id_populasi">
+                        class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] focus:shadow-[2px_2px_0px_2px_#374151] focus:translate-y-0.5 focus:translate-x-0.5 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-gray-700 text-gray-700 leading-5 transition duration-150 ease-in-out block mt-1 w-full py-2.5
+                        required x-model="editData.id_populasi">
                         <option value="" disabled>Pilih Batch</option>
                         @foreach ($batches as $batch)
                             <option value="{{ $batch->id }}">{{ $batch->nama_batch }}</option>
@@ -60,7 +59,7 @@
             <!-- Tombol Update -->
             <div class="flex items-center justify-end">
                 <x-primary-button
-                    class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] text-white hover:shadow-[2px_2px_0px_2px_#374151] hover:translate-y-0.5 hover:translate-x-0.5 w-1/2 text-center bg-orangeCrayola">
+                    class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] text-white hover:shadow-[2px_2px_0px_2px_#374151] hover:translate-y-0.5 hover:translate-x-0.5 w-1/2 text-center bg-orangeCrayola py-2.5">
                     {{ __('Perbarui Data') }}
                 </x-primary-button>
             </div>
--- resources/views/components/popup-form-edit-jumlah-ayam.blade.php
+++ resources/views/components/popup-form-edit-jumlah-ayam.blade.php
@@ -1,12 +1,15 @@
 <!-- resources/views/components/popup-form-edit-jumlah-ayam.blade.php -->
-@props(['editData']) <!-- Mendefinisikan props 'editData' -->
-
-<div
-    x-show="openModal === 'editJumlahAyam'"
+<div 
+    x-data="{ openModal: null, editData: {} }"
+    @open-edit-populasi.window="
+        openModal = 'editPopulasiAyam'; 
+        editData = $event.detail;
+    "
+    x-show="openModal === 'editPopulasiAyam'"
     x-cloak
     class="fixed inset-0 flex items-center justify-center z-50 bg-black/15"
-    x-transition
->
+    x-transition>
+    
     <div class="bg-white w-1/3 p-6 rounded-lg shadow-lg relative">
         <!-- Tombol Close -->
         <button @click="openModal = null"
@@ -18,82 +21,70 @@
         <h2 class="text-2xl font-semibold mb-4">Edit Data Populasi Ayam</h2>
 
         <!-- Form Edit -->
-        <form 
-            id="editBatchForm" 
-            method="POST" 
-            @submit.prevent="submitEdit"
-        >
+        <form method="POST" :action="`/populasi/${editData.id}`">
             @csrf
-            @method('PUT') <!-- Menetapkan metode HTTP ke PUT untuk update -->
+            @method('PUT')
 
             <div class="space-y-4 mb-8">
-                <!-- Input Kode Batch (Static "BATCH-" dan Editable Suffix) -->
+                <!-- Pilih Kandang -->
+                <div>
+                    <x-input-label for="kandang_id" :value="__('Pilih Kandang')" />
+                    <select id="kandang_id" name="kandang_id" class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] focus:shadow-[2px_2px_0px_2px_#374151] focus:translate-y-0.5 focus:translate-x-0.5 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-gray-700 text-gray-700 leading-5 transition duration-150 ease-in-out block mt-1 w-full py-2.5" required x-model="editData.kandang_id">
+                        <option value="" disabled selected>Pilih Kandang</option>
+                        @foreach ($kandang as $item)
+                            <option value="{{ $item->id }}">{{ $item->nama_kandang }} ({{ $item->kapasitas }} Kapasitas)</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <!-- Input Kode Batch -->
                 <div>
                     <x-input-label for="batchCodeSuffix" :value="__('Kode Batch')" />
-                    <div class="flex">
-                        <span class="flex items-center px-3 bg-gray-200 text-gray-700 border border-r-0 border-gray-300 rounded-l-md">
-                            BATCH-
-                        </span>
-                        <x-text-input 
-                            id="batchCodeSuffix" 
-                            name="batchCodeSuffix" 
-                            type="text"
-                            class="flex-1 block mt-1 w-full py-2.5 border border-gray-300 rounded-r-md"
-                            required 
-                            pattern="\d{3}" 
-                            title="Masukkan 3 digit angka"
-                            x-model="editData.batchCodeSuffix" 
-                            placeholder="001"
-                        />
+                    <div class="flex space-x-4">
+                        <x-text-input id="batchPrefix" name="batchPrefix" type="text" class="w-1/4" value="BATCH-" readonly />
+                        <x-text-input id="batchCodeSuffix" name="batchCodeSuffix" type="text" class="w-full" required
+                                      x-model="editData.batchCodeSuffix"  pattern="[a-zA-Z0-9]{3}" maxlength="3" 
+                                      title="Masukkan kombinasi huruf dan angka, maksimal 3 karakter" 
+                                      placeholder="A01" uppercase/>
                     </div>
                 </div>
 
                 <!-- Input Nama Batch -->
                 <div>
-                    <x-input-label for="batchName" :value="__('Nama Batch')" />
-                    <x-text-input 
-                        id="batchName" 
-                        name="batchName" 
-                        type="text"
-                        class="block mt-1 w-full py-2.5" 
-                        required 
-                        x-model="editData.nama_batch" 
-                    />
+                    <x-input-label for="nama_batch" :value="__('Nama Batch')" />
+                    <x-text-input id="nama_batch" name="nama_batch" type="text" class="block mt-1 w-full py-2.5"
+                                  required x-model="editData.nama_batch"/>
                 </div>
 
                 <!-- Input Tanggal DOC -->
                 <div>
-                    <x-input-label for="docDate" :value="__('Tanggal DOC')" />
-                    <x-text-input 
-                        id="docDate" 
-                        name="docDate" 
-                        type="date"
-                        class="block mt-1 w-full py-2.5" 
-                        required 
-                        x-model="editData.tanggal_doc" 
-                    />
+                    <x-input-label for="tanggal_doc" :value="__('Tanggal DOC')" />
+                    <x-text-input id="tanggal_doc" name="tanggal_doc" type="date" class="block mt-1 w-full py-2.5"
+                                  required x-model="editData.tanggal_doc" />
                 </div>
 
                 <!-- Input Jumlah Ayam Masuk -->
                 <div>
-                    <x-input-label for="chickenQuantity" :value="__('Jumlah Ayam Masuk')" />
-                    <x-text-input 
-                        id="chickenQuantity" 
-                        name="chickenQuantity" 
-                        type="number"
-                        class="block mt-1 w-full py-2.5" 
-                        required 
-                        x-model="editData.jumlah_ayam_masuk" 
-                    />
+                    <x-input-label for="jumlah_ayam_masuk" :value="__('Jumlah Ayam Masuk')" />
+                    <x-text-input id="jumlah_ayam_masuk" name="jumlah_ayam_masuk" type="number"
+                                  class="block mt-1 w-full py-2.5" required x-model="editData.jumlah_ayam_masuk" />
+                </div>
+
+                <!-- Pilih Status Ayam -->
+                <div>
+                    <x-input-label for="status_ayam" :value="__('Status Ayam')" />
+                    <select id="status_ayam" name="status_ayam" class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] focus:shadow-[2px_2px_0px_2px_#374151] focus:translate-y-0.5 focus:translate-x-0.5 rounded-md focus:outline-none focus:border-none focus:ring-2 focus:ring-gray-700 text-gray-700 leading-5 transition duration-150 ease-in-out block mt-1 w-full py-2.5 required x-model="editData.status_ayam">
+                        <option value="" disabled>Pilih Status Ayam</option>
+                        <option value="Proses">Proses</option>
+                        <option value="Siap Panen">Siap Panen</option>
+                        <option value="Sudah Panen">Sudah Panen</option>
+                    </select>
                 </div>
             </div>
 
             <!-- Tombol Update -->
             <div class="flex items-center justify-end">
-                <x-primary-button
-                    id="submitButton"
-                    class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] text-white hover:shadow-[2px_2px_0px_2px_#374151] hover:translate-y-0.5 hover:translate-x-0.5 w-1/2 text-center bg-orangeCrayola"
-                >
+                <x-primary-button class="ring-2 ring-gray-700 shadow-[4px_4px_0px_2px_#374151] text-white hover:shadow-[2px_2px_0px_2px_#374151] hover:translate-y-0.5 hover:translate-x-0.5 w-1/2 text-center bg-orangeCrayola py-2.5">
                     {{ __('Perbarui Data') }}
                 </x-primary-button>
             </div>
