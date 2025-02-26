056caeccfbbaf015c9bc88d86161e9444474ad3c
KahfiSmith <alkahfii2018@gmail.com>
2025-02-26 16:55:11 +0700

    feat: role admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Providers\RouteServiceProvider;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,8 +14,32 @@ class AuthenticatedSessionController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('store');
     }
+
+    /**
+     * Handle login and redirect based on role.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+{
+    $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($request->only('email', 'password'), $request->boolean('remember'))) {
+        $request->session()->regenerate();
+
+        return redirect()->intended(RouteServiceProvider::HOME); // 🔥 Redirect otomatis sesuai role
+    }
+
+    return back()->withErrors([
+        'email' => 'Kredensial yang diberikan salah.',
+    ]);
+}
 
     /**
      * Logout the authenticated user and invalidate their session.
