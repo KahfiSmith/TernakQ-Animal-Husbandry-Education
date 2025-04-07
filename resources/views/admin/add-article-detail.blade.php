c78da8b1bb9e3922f18a9b1e8a4bc6ba428d1a59
KahfiSmith <alkahfii2018@gmail.com>
2025-04-07 20:35:24 +0700

    fix: logic get tags

--- resources/views/admin/add-article-detail.blade.php
+++ resources/views/admin/add-article-detail.blade.php
@@ -135,8 +135,7 @@
                     <div id="tags-dropdown" style="display: none;" class="absolute w-full mt-1 bg-white border-2 border-gray-300 rounded-md shadow-lg z-10">
                         @foreach($tags as $tag)
                             <label class="block px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer">
-                                <input type="checkbox" name="tags[]" value="{{ $tag->id }}" 
-                                    onclick="updateSelectedTags({{ $tag->id }}, this)" class="mr-2">
+                                <input type="checkbox" name="tags[]" value="{{ $tag->id }}" x-model="tags" class="mr-2">
                                 {{ $tag->name }}
                             </label>
                         @endforeach
